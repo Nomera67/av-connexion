04991761a475664e3f148e25e92ad37438eb2cc8
Nomera67 <y.rechtenstein@gmail.com>
2023-10-16 12:04:52 +0200

    Ajout de la manière de mettre le header en position fixed. Pas encore au point

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, ElementRef } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 
 @Component({
@@ -6,10 +6,41 @@ import { ViewportScroller } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  constructor(private viewportScroller: ViewportScroller) {}
+export class NavbarComponent implements OnInit{
+
+  opacity: number = 0;
+
+  ngOnInit(): void {
+  }
+
+
+  constructor(private elementRef: ElementRef, private viewportScroller: ViewportScroller) {
+  }
 
   scrollToElement(elementId: string): void {
     this.viewportScroller.scrollToAnchor(elementId)
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const headerBottom = this.elementRef.nativeElement.querySelector('.header-bottom') as HTMLElement;
+
+
+    if(headerBottom){
+      const rect = headerBottom.getBoundingClientRect();
+      const topScreen = rect.top;
+      if (topScreen <= 0) {
+        headerBottom.style.position = 'fixed';
+        headerBottom.style.top = '0';
+      } else {
+        headerBottom.style.position = 'relative';
+      }
+    }
+
+
+    const scrollPosition = window.pageYOffset;
+    const maxScroll = document.body.clientHeight - window.innerHeight;
+    const opacity = (scrollPosition / maxScroll) * 10;
+    this.opacity = opacity < 0 ? 0 : (opacity > 1 ? 1 : opacity);
   }
 }
