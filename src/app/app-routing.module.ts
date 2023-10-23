49d692e89c4133da6b9a62946bfcbf929a9ad1b3
Nomera67 <y.rechtenstein@gmail.com>
2023-10-23 11:51:37 +0200

    Ajout de la page Labo, correction du fonctionnement de la navbar en fonction de la nouvelle page, ajout d'un effet de couleur derrière le chat de la homepage, correction de certains fonctionnement, automatisation de l'onglet team et de l'onglet certification avec ajout de JSON plus simples à maintenir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/accueil'},
   { path: 'accueil', loadChildren: () => import('./features/components/accueil/accueil.module').then(m => m.AccueilModule) },
+  { path: 'laboratoire', loadChildren: () => import('./features/components/labo/labo.module').then(m => m.LaboModule) },
   { path: '**', loadChildren: () => import('./shared/not-found/not-found.module').then(m => m.NotFoundModule) }
 ];
 
--- src/app/features/components/accueil/certifications/certifications.component.ts
+++ src/app/features/components/accueil/certifications/certifications.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-certifications',
   templateUrl: './certifications.component.html',
   styleUrls: ['./certifications.component.scss']
 })
-export class CertificationsComponent {
+export class CertificationsComponent implements OnInit {
+
+  certifications: any[] = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('assets/ressources/certifications/certifications-list.json').subscribe((data: any) => {
+      this.certifications = data;
+    })
+
+    
+  }
 
 }
